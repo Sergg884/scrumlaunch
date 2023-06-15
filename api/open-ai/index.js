90fb0b33659742c707dd9a8c78c617e4d5629bff
Michael Shatskiy <122393649+ten-mxs@users.noreply.github.com>
2023-06-15 18:39:25 +0200

    Added errors handler with retries

--- api/open-ai/index.js
+++ api/open-ai/index.js
@@ -11,18 +11,30 @@ const openai = new OpenAIApi(configuration);
 // const OPENAI_MODEL = "gpt-4";
 const OPENAI_MODEL = "gpt-3.5-turbo";
 
-function makeRequest(messages, temperature = 0.5) {
-  return new Promise((resolve, reject) => {
-    openai.createChatCompletion({
-      model: OPENAI_MODEL,
-      messages,
-      temperature
-    }).then((response) => {
-      resolve(response.data.choices[0].message.content)
-    }).catch((error) => {
-      reject(error);
-    });
-  });
+async function makeRequest(messages, temperature = 0.5, retries = 5, retryDelay = 10) {
+
+  let currentRetry = 0;
+
+  async function callApi() {
+    try {
+      const result = await openai.createChatCompletion({
+        model: OPENAI_MODEL,
+        messages,
+        temperature
+      });
+      return(JSON.parse(result.data.choices[0].message.content));
+    } catch(error) {
+      if (currentRetry < retries) {
+        await utils.delay(retryDelay * 1000);
+        currentRetry++;
+        return await callApi();
+      } else {
+        throw new Error({error: "Something bad happened"});
+      }
+    }
+  }
+
+  return await callApi();
 }
 
 async function start(projectDescription, stepCallbackFn, resultCallbackFn) {
@@ -37,60 +49,55 @@ async function start(projectDescription, stepCallbackFn, resultCallbackFn) {
     )
   }]);
 
-  const technicalTaskToJSON = JSON.parse(technicalTask)
   console.log(technicalTask)
 
-  resultCallbackFn({ data: technicalTaskToJSON, type: "details" });
+  resultCallbackFn({ data: technicalTask, type: "details" });
   stepCallbackFn("Fetching technologies...");
 
   const technologies = await makeRequest([{
     role: "user",
     content: utils.cleanString(
       prompts.getListOfTechnologies(
-        utils.cleanString(technicalTask)
+        utils.cleanString(JSON.stringify(technicalTask))
       )
     )
   }], 0);
 
-  const technologiesToJSON = JSON.parse(technologies)
   console.log(technologies)
 
-  resultCallbackFn({ data: technologiesToJSON, type: "technologies" });
+  resultCallbackFn({ data: technologies, type: "technologies" });
   stepCallbackFn("Fetching estimate...");
 
   const estimate = await makeRequest([{
     role: "user",
     content: utils.cleanString(
       prompts.getEstimate(
-        utils.cleanString(technicalTask)
+        utils.cleanString(JSON.stringify(technicalTask))
       )
     )
   }]);
 
-  const estimateToJSON = JSON.parse(estimate)
   console.log(estimate)
 
-  resultCallbackFn({ data: estimateToJSON, type: "estimate" });
+  resultCallbackFn({ data: estimate, type: "estimate" });
   stepCallbackFn("Fetching team composition...");
 
   const teamComposition = await makeRequest([{
     role: "user",
     content: utils.cleanString(
       prompts.getTeamComsition(
-        utils.cleanString(technologies),
-        utils.cleanString(estimate)
+        utils.cleanString(JSON.stringify(technologies)),
+        utils.cleanString(JSON.stringify(estimate))
       )
     )
   }], 0);
 
-
-  const teamCompositionToJson = JSON.parse(teamComposition)
   console.log(teamComposition)
 
-  resultCallbackFn({ data: teamCompositionToJson, type: "teamComposition" });
+  resultCallbackFn({ data: teamComposition, type: "teamComposition" });
   stepCallbackFn("Fetching SL team...");
 
-  const fetchTeamResult = await fetchTeam({ teamComposition: teamCompositionToJson });
+  const fetchTeamResult = await fetchTeam({ teamComposition });
 
   stepCallbackFn("Done");
   resultCallbackFn({ data: fetchTeamResult?.teamComposition, type: "slTeamComposition", finished: true });
