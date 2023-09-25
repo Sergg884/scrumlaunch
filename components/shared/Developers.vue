b4bf4d475b27244dd4bf747ea14f95433dec9dcc
Dmitriy <dima.khrystych@gmail.com>
2023-09-25 14:10:35 +0300

    updated developers blocks

--- components/shared/Developers.vue
+++ components/shared/Developers.vue
@@ -1,38 +1,46 @@
 <template>
-  <div class="talent-map" :class="{'white': isWhite}" >
+  <div class="developers" :class="{'white': isWhite}" >
     <section>
       <h3>{{devLang}} developers</h3>
-      <b-row class="text-container" no-gutters>
-        <div 
+      <b-row no-gutters>
+        <b-col
           v-for="(dev, index) in developers"
           :key="index"
+          class="developer col-12"
         >
-          <nuxt-img :src="'/' + dev.avatar_url" alt="candidate avatar" />
-          <span>{{dev.name}}</span>
-          <span>{{dev.exp}}</span>
-          <span>{{dev.english_level}}</span>
-          <div v-for="skill in dev.candidate_hard_skills" :key="skill.id">
-            <div>
+          <b-row class="flex">
+            <nuxt-img class="developer-avatar" :src="'/' + dev.avatar_url" alt="candidate avatar" />
+            <div class="">
+              <p class="developer-name">{{dev.name}}</p>
+              <span class="developer-exp">{{dev.exp}} years of exp.</span>
+              <span class="developer-eng">Eng: {{dev.english_level}}</span>
+            </div>
+          </b-row>
+          <b-row class="developer--hard-skills">
+            <span class="developer-skill">Hard skill(s):</span>
+            <div v-for="skill in dev.candidate_hard_skills" :key="skill.id">
               <nuxt-img :src="'/' + skill.stack.icon_s3_key" />
-              <span>{{skill.stack.name}}</span>
-              <span>{{ skill.assessment_result }}</span>
+              <span class="developer--stack-name">{{skill.stack.name}}</span>
+              <span>{{ skill.assessment_result }}%</span>
             </div>
-          </div>
-          <div v-for="skill in dev.candidate_soft_skills" :key="skill.id">
-            <div>
+          </b-row>
+          <b-row class="developer--soft-skills">
+            <span class="developer-skill">Soft skill(s):</span>
+            <div v-for="skill in dev.candidate_soft_skills" :key="skill.id">
+              <nuxt-img :src="skill.skill.s3_key || '/'" />
+              <span class="developer--skill-name">{{skill.skill.name}}</span>
+              <span>{{ skill.assessment_result }}%</span>
+            </div>
+          </b-row>
+          <b-row class="developer--additional-skills">
+            <span class="developer-skill">Additional:</span>
+            <div v-for="skill in dev.candidate_additional_skills" :key="skill.id" class="flex no-wrap">
               <nuxt-img :src="skill.skill.s3_key || '/'" />
               <span>{{skill.skill.name}}</span>
-              <span>{{ skill.assessment_result }}</span>
+              <span>{{ skill.assessment_result }}%</span>
             </div>
-          </div>
-          <div v-for="skill in dev.candidate_additional_skills" :key="skill.id">
-            <div>
-              <nuxt-img :src="skill.skill.s3_key || '/'" />
-              <span>{{skill.skill.name}}</span>
-              <span>{{ skill.assessment_result }}</span>
-            </div>
-          </div>
-        </div>
+          </b-row>
+        </b-col>
         <!-- <div class="text-block">
           <h3>The best talent from around the globe at your fingertips</h3>
           <p>ScrumLaunch has talent hubs with management teams on the ground in North America, Latin America, Eastern Europe and India to facilitate deep access to each local market.</p>
@@ -80,7 +88,7 @@ export default {
     const {data} = await this.$axios({
       method: 'get',
       url: this.$config.scrumTeamsAPI,
-      headers: { 'HTTP-API-KEY': 123123 },
+      headers: { 'HTTP-API-KEY': 'adb27cb0ad4b9c3534a4f520' },
     });
 
     this.developers = data.candidates || [];
@@ -93,23 +101,80 @@ export default {
 
 <style lang="scss" scoped>
 
-.talent-map {
+.developers {
   display: flex;
 
   h3 {
     text-transform: uppercase;
-
     @include tablet-and-up {
-    margin: 0 15px;
-  }
-
+      margin: 0 15px;
+    }
     @include desktop-and-up {
       margin: 0 40px;
     }
   }
 
-  .map-image {
-    width: 100%;
+  .developer {
+    height: 366px;
+    box-sizing: border-box;
+    border: 1px solid black;
+    padding: 28.854px 37.51px;
+    margin-bottom: 20px;
+
+    @include tablet-and-up {
+      height: 300px;
+    }
+    @include desktop-and-up {
+      height: 166px;
+    }
+
+    &-avatar {
+      width: 115px;
+      height: 115px;
+    }
+
+    &-name {
+      text-align: left;
+      color: var(--primary-colors-black, #121212);
+      font-family: Inter;
+      font-size: 26px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 140%; /* 36.4px */
+    }
+
+    &-exp, &-eng {
+      color: var(--basic-colors-dark-gray, #726C7A);
+      font-family: Inter;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 150%; /* 30px */
+    }
+
+    &-skill {
+      color: var(--basic-colors-dark-gray, #726C7A);
+      font-family: Inter;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 150%; /* 24px */
+    }
+
+    &--stack-name, &--skill-name {
+      color: var(--primary-colors-black, #121212);
+      font-family: Inter;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 150%; /* 30px */
+    }
+
+    &--additional-skills, &--hard-skills, &--soft-skills {
+      display: flex;
+      align-items: center;
+      height: 45px;
+    }
   }
 }
 
